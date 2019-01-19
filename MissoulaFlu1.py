8545564ed42bb425a20a9ed31a2c7a6064fd1b27
mandub <mandoob1@hotmail.com>
2019-01-19 16:01:08 -0700

    add pdf file and some code

--- MissoulaFlu1.py
+++ MissoulaFlu1.py
@@ -10,9 +10,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
-
-path = r"C:\Users\annag\Documents\2018-2019\Spring_2019\BigDataProjects\Mslaflu_1.csv"
-
+path =""
+path1 = r"C:\Users\annag\Documents\2018-2019\Spring_2019\BigDataProjects\Mslaflu_1.csv"
+path2 = "/home/mandub/Desktop/6th semester/courses/Data Science Projects/data flu/flu-data-linear-regression/montana_flu_compiled_master_weekly INITIAL SAMPLE to team2.xlsx"
+try:
+    with open(path, encoding = "utf-8") as f:
+        print ("Ok")
+    path = path1  
+except:
+    print "This is an error message!"
+try:
+    with open(path, encoding = "utf-8") as f:
+        print ("Ok")
+    path = path2  
+except:
+    print "this is not mandub"
+    
 MslaDict = defaultdict(list)
 with open(path, encoding = "utf-8") as f:
     next(f)         #skip the header
