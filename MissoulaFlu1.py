0d2e267c995b2ad55f42fff80a2efcb25f833da6
Anna <anna.gasner@umontana.edu>
2019-01-19 16:14:55 -0700

    changed path

--- MissoulaFlu1.py
+++ MissoulaFlu1.py
@@ -11,20 +11,22 @@ import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 path =""
-path1 = r"C:\Users\annag\Documents\2018-2019\Spring_2019\BigDataProjects\Mslaflu_1.csv"
+path1 = r"C:\Users\annag\Documents\2018-2019\Spring_2019\BigDataProjects\flu-data-linear-regression\Mslaflu_1.csv"
 path2 = "/home/mandub/Desktop/6th semester/courses/Data Science Projects/data flu/flu-data-linear-regression/montana_flu_compiled_master_weekly INITIAL SAMPLE to team2.xlsx"
 try:
-    with open(path, encoding = "utf-8") as f:
-        print ("Ok")
-    path = path1  
+    
+ 
+    with open(path1, encoding = "utf-8") as f:
+        print ("Ok")   
+        path = path1
 except:
-    print "This is an error message!"
-try:
-    with open(path, encoding = "utf-8") as f:
+    print ("This is an error message!")
+try:   
+    with open(path2, encoding = "utf-8") as f:
         print ("Ok")
-    path = path2  
+        path = path2      
 except:
-    print "this is not mandub"
+    print ("this is not mandub")
     
 MslaDict = defaultdict(list)
 with open(path, encoding = "utf-8") as f:
