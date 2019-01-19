893a119d670723b8bd7043725407b6efde5bb4bc
Jake <jakeoetinger@gmail.com>
2019-01-19 16:24:37 -0700

    added my path3

--- MissoulaFlu1.py
+++ MissoulaFlu1.py
@@ -10,9 +10,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
-path =""
+
 path1 = r"C:\Users\annag\Documents\2018-2019\Spring_2019\BigDataProjects\flu-data-linear-regression\Mslaflu_1.csv"
 path2 = "/home/mandub/Desktop/6th semester/courses/Data Science Projects/data flu/flu-data-linear-regression/montana_flu_compiled_master_weekly INITIAL SAMPLE to team2.xlsx"
+path3 = r"C:\Users\jakeo\OneDrive\Documents\M467\flu-data-linear-regression\Mslaflu_1.csv"
 try:
     
  
@@ -27,6 +28,13 @@ try:
         path = path2      
 except:
     print ("this is not mandub")
+
+try:   
+    with open(path3, encoding = "utf-8") as f:
+        print ("Ok")
+        path = path3      
+except:
+    print ("this is not Jake")
     
 MslaDict = defaultdict(list)
 with open(path, encoding = "utf-8") as f:
